caf101ad5a30a923a2e86aa28326e33810a2f91b
Alex Yankov <yankov.alex@gmail.com>
2025-01-16 22:49:16 -0500

    uv and poetry now work

--- tests/resources/assets_py/main.py
+++ tests/resources/assets_py/main.py
@@ -5,5 +5,4 @@ def main_build():
     print("Build task executed successfully")
 
 if __name__ == "__main__":
-    # This allows the file to be run directly for testing
-    main_test() 
+    main()
