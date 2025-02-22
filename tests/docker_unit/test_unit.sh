2c7d806a588ad6d30cbfbfb73eb4ac1c0b03af41
Alex Yankov <yankov.alex@gmail.com>
2025-02-22 15:38:04 -0500

    tests fail

--- tests/docker_unit/test_unit.sh
+++ tests/docker_unit/test_unit.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 set -e
 
-# Get the directory containing this script
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PROJECT_ROOT="$SCRIPT_DIR/.."
-
-# Build the Docker image
-docker build -t dela-unit-tests -f "$SCRIPT_DIR/Dockerfile" "$PROJECT_ROOT"
-
-# Run the tests in a container
-docker run --rm dela-unit-tests 
+# Run all tests
+cargo test --all-features --verbose 
