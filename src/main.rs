00d416911b0f118eec1d222a79f580b1122c5629
Alex Yankov <yankov.alex@gmail.com>
2025-04-16 14:50:57 -0400

    text

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ mod types;
     about = "A task runner that delegates to others",
     long_about = r#"Dela scans your project directory for task definitions in various formats (Makefile, package.json, etc.) and lets you run them directly from your shell.
 
-After running '$ dela init', you can:
+After initializing once with '$ dela init', you can:
 1. Execute task with bare name `$ <task>` through the shell integration
 2. Use '$ dr <task>' to execute a task explicitly"#
 )]
