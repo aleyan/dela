03a5251a4b83f9a09cb35301a732b23160f0b73d
Alex Yankov <yankov.alex@gmail.com>
2025-04-16 14:47:35 -0400

    fixes?

--- tests/docker_unit/test_unit.sh
+++ tests/docker_unit/test_unit.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
 
-# Run all tests using pre-compiled dependencies
-cargo test --all-features --verbose 
+# Run all tests using the pre-compiled dependencies
+echo "Running unit tests..."
+cargo test --all-features --verbose
+echo "Unit tests completed successfully!" 
